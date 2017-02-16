a9006e40cef83c1a1cd3fdb997d9c254cabddbf9
Coty Hamilton <cotyhamilton@gmail.com>
2017-02-15 22:18:18 -0600

    final

--- Assignments/war.py
+++ Assignments/war.py
@@ -1,4 +1,13 @@
 # -*- coding: utf-8 -*-
+
+# Name: Coty Hamilton
+
+# Email: cotyhamilton@gmail.com
+
+# Assignment: Homework 2 - War
+
+# Due: 17 Feb @ 11:59 p.m.
+
 import os
 import random
 import time
@@ -43,25 +52,7 @@ class Card(object):
         :param rank: The value of the card, e.g 3 or King
         """
 
-        self.ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King","Ace"]
-
-
-
-        self.card_values = {
-            '2': 2,
-            '3': 3,
-            '4': 4,
-            '5': 5,
-            '6': 6,
-            '7': 7,
-            '8': 8,
-            '9': 9,
-            '10': 10,
-            'Jack': 11,
-            'Queen': 12,
-            'King': 13,
-            'Ace': 14,  # value of the ace is high until it needs to be low
-        }
+        self.ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K","A"]
 
         self.str_values = {
             '2': CARD,
@@ -73,10 +64,10 @@ class Card(object):
             '8': CARD,
             '9': CARD,
             '10': TEN,
-            'Jack': CARD,
-            'Queen': CARD,
-            'King': CARD,
-            'Ace': CARD,
+            'J': CARD,
+            'Q': CARD,
+            'K': CARD,
+            'A': CARD,
         }
 
         self.suits = ['Spades','Hearts','Diamonds','Clubs']
@@ -88,32 +79,29 @@ class Card(object):
             'Clubs':    '♣',
         }
 
-
         if type(suit) is int:
             self.suit = self.suits[suit]
         else:
             self.suit = suit.capitalize()
         self.rank = str(rank)
         self.symbol = self.symbols[self.suit]
-        self.points = self.card_values[str(rank)]
+        self.points = str(rank).index(self.rank)
         self.ascii = self.__str__()
     
-
     def __str__(self):
         symbol = self.symbols[self.suit]
-        trank = self.rank[0]+symbol
-        brank = symbol+self.rank[0]
+        trank = self.rank+symbol
+        brank = symbol+self.rank
         return self.str_values[self.rank].format(trank=trank, suit=symbol,brank=brank)
-           
-    def __cmp__(self,other):
-        
-        return self.ranks.index(self.rank) < self.ranks.index(other.rank) 
-   
-    # Python3 wasn't liking the __cmp__ to sort the cards, so 
-    # documentation told me to use the __lt__ (less than) 
-    # method.
+
+# overload comparison operators for card objects
+
     def __lt__(self,other):
-        return self.__cmp__(other)
+        return self.ranks.index(self.rank) < self.ranks.index(other.rank)
+    def __gt__(self,other):
+        return self.ranks.index(self.rank) > self.ranks.index(other.rank)
+    def __eq__(self,other):
+        return self.ranks.index(self.rank) == self.ranks.index(other.rank)
 
 """
 @Class Deck 
@@ -130,7 +118,7 @@ class Deck(list):
         #assume top of deck = 0th element
         self.cards = []
         for suit in range(4):
-            for rank in ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King","Ace"]:
+            for rank in ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K","A"]:
                 self.cards.append(Card(suit,rank))
                 
     def __str__(self):
@@ -145,6 +133,8 @@ class Deck(list):
     def add_card(self,card):
         self.cards.append(card)
         
+# random.shuffle randomizes order of objects in a list
+
     def shuffle(self):
         random.shuffle(self.cards)
     
@@ -181,86 +171,135 @@ class Hand(list):
     
     def pop_card(self):
         return self._list.pop(0)
-    
-    def __getitem__(self,key):
-        return self._list[key]
 
+# random.shuffle randomizes order of objects in a list
+
+    def shuffle(self):
+        random.shuffle(self._list)
+
+"""
+@Class Game
+@Description:
+    This class represents a game. 
+@Methods:
+    deal() - deals 26 cards to each player
+    compare() - compares ranks of two cards
+    emptyPot() - distributes cards from pot to winner and emptys each players temporary war hand
+    tie() - adds cards to pot and temporary war hand
+""" 
 class Game(object):
     def __init__(self,player_name):
         self.D = Deck()
         self.D.shuffle()
-        self.D.shuffle()
-        self.computer = {"name": "Computer", "hand":Hand()}
-        self.player1 = {"name": player_name, "hand":Hand()}
+
+        self.computer = Player("COMPUTER")
+        self.player = Player(player_name)
+
+
         self.winner = None
+        self.count = 0
+        self.warCount = 0
         self.pot = Hand()
         self.__deal()
         self.__gameloop()
     
     def __deal(self):
-        for i in range(4):
-            self.computer["hand"].add(self.D.pop_card())
-            self.player1["hand"].add(self.D.pop_card())
+        for i in range(26):
+            self.computer.hand.add(self.D.pop_card())
+            self.player.hand.add(self.D.pop_card())
 
-    # def __cmp__(self):
-    #     return self.computer["hand"][0] < self.player1["hand"][0]
-    
-    # def __lt__(self):
-    #     return self.__cmp__()
+    def __compare(self, playerCard, computerCard):
+        if(playerCard > computerCard):
+            self.win = self.player
+        elif (playerCard < computerCard):
+            self.win = self.computer
+        else:
+            self.win = "tie"
+        return self.win
+
+    def __emptyPot(self, winningPlayer):
+        while(len(self.pot._list)):
+            winningPlayer.hand.add(self.pot.pop_card())
+        while(len(self.player.war._list)):
+            self.player.war.pop_card()
+        while(len(self.computer.war._list)):
+            self.computer.war.pop_card()
+
+    def __tie(self, p, c):
+        os.system('clear')
+        print("██╗    ██╗ █████╗ ██████╗\n██║    ██║██╔══██╗██╔══██╗\n██║ █╗ ██║███████║██████╔╝\n██║███╗██║██╔══██║██╔══██╗\n╚███╔███╔╝██║  ██║██║  ██║\n ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝\n")
+        
+        # only if there is more than one card in a player's hand
+        
+        # exits after popping two cards in to each players war hand and the main pot
+
+        # game continues as normal in main game loop
+        
+        if(len(c.hand._list) > 1):
+            c.war.add(c.hand._list[0])
+            self.pot.add(c.hand.pop_card())
+        if(len(c.hand._list) > 1):
+            c.war.add(c.hand._list[0])
+            self.pot.add(c.hand.pop_card())
+
+        if(len(p.hand._list) > 1):
+            p.war.add(p.hand._list[0])
+            self.pot.add(p.hand.pop_card())
+        if(len(p.hand._list) > 1):
+            p.war.add(p.hand._list[0])
+            self.pot.add(p.hand.pop_card())
+
+        print(c.name + "'s hand:\n")
+        print(c.war)
+        print()
+        print(p.name + "'s hand:")
+        print(p.war)
+        print()
+
+        self.warCount = self.warCount + 1
+
 
     def __gameloop(self):
-        while(len(self.computer["hand"]._list) and len(self.player1["hand"]._list)):
-            print(self.computer["hand"]._list[0])
-            print(self.player1["hand"]._list[0])
+        while(len(self.computer.hand._list) and len(self.player.hand._list)):
+            os.system('clear')
+            print(" ██████╗  █████╗ ███╗   ███╗███████╗\n██╔════╝ ██╔══██╗████╗ ████║██╔════╝\n██║  ███╗███████║██╔████╔██║█████╗\n██║   ██║██╔══██║██║╚██╔╝██║██╔══╝ \n╚██████╔╝██║  ██║██║ ╚═╝ ██║███████╗\n ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚══════╝")
+            print()
+            print(self.computer.name)
+            print()
+            print(self.computer.hand._list[0])
+            print(self.player.hand._list[0])
+            print()
+            print(self.player.name)
+            print()
 
-
-            if(self.player1["hand"]._list[0] < self.computer["hand"]._list[0]):
-                self.winner = "player1"
-            elif (self.player1["hand"]._list[0] > self.computer["hand"]._list[0]):
-                self.winner = "computer"
+            self.winner = self.__compare(self.player.hand._list[0], self.computer.hand._list[0])
+            
+            if (self.winner == "tie"):
+                print("██╗    ██╗ █████╗ ██████╗\n██║    ██║██╔══██╗██╔══██╗\n██║ █╗ ██║███████║██████╔╝\n██║███╗██║██╔══██║██╔══██╗\n╚███╔███╔╝██║  ██║██║  ██║\n ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝\n")
+                self.__tie(self.player, self.computer)
             else:
-                self.winner = "tie"
-                print("tie")
-            self.pot.add(self.player1["hand"].pop_card())
-            self.pot.add(self.computer["hand"].pop_card())
+                self.pot.add(self.player.hand.pop_card())
+                self.pot.add(self.computer.hand.pop_card())
+                self.__emptyPot(self.winner)
+                print(self.winner.name + " wins")
 
-            if(self.winner == "player1"):
-                while(len(self.pot._list)):
-                    self.player1["hand"].add(self.pot.pop_card())
-                print("player 1 wins")
-            elif(self.winner == "computer"):
-                while(len(self.pot._list)):
-                    self.computer["hand"].add(self.pot.pop_card())
-                print("computer wins")
+            self.count = self.count + 1
 
-            print("p1 hand:")
-            print(self.player1["hand"])
-            print("computer hand:")
-            print(self.computer["hand"])
-            time.sleep(2)
+            if (self.count % 26 == 0):
+                self.player.hand.shuffle()
+                self.computer.hand.shuffle()
+            
+            # time.sleep(.75)
 
             
-
-
-
 class Player(object):
-    def __init__(self,name,hand):
+    def __init__(self,name):
         self.name = name
-        self.hand = hand
-
-    
-
-# H = Hand()
-# D = Deck()
-# D.shuffle()
-# for i in range(5):
-#     H.add(D.pop_card())
-# H.sort()
-# print(H)
-
-G = Game("Coty")
-# print(G.computer["hand"])
-# print(G.player1["hand"])
+        self.hand = Hand()
+        self.war = Hand()
 
 
-
+G = Game("PLAYER")
+os.system('clear')
+print ("\n\n\t\t" + G.winner.name + " WINS THE GAME!!")
+print ("\n\n\t\tWar occured " + str(G.warCount) + " times out of " + str(G.count) + " rounds")
